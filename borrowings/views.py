0c82580891c7983fe6b15cefb6a49fa5582289f9
Oleh M <oleh.murachov28@gmail.com>
2026-09-21 00:04:24 +0200

    feat: add return borrowing endpoint

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,7 +1,9 @@
 from django.db import transaction
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from books.models import Book
 from borrowings.models import Borrowing
@@ -57,3 +59,37 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         book.save(update_fields=["inventory"])
 
         serializer.save(user=self.request.user)
+
+    @action(
+        detail=True,
+        methods=["post"],
+        url_path="return",
+    )
+    @transaction.atomic
+    def return_borrowing(self, request, pk=None):
+        borrowing = self.get_object()
+
+        if borrowing.actual_return_date is not None:
+            return Response(
+                {"detail": "This borrowing has already been returned."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        borrowing.actual_return_date = request.data.get(
+            "actual_return_date"
+        )
+
+        if borrowing.actual_return_date is None:
+            from django.utils import timezone
+
+            borrowing.actual_return_date = timezone.now().date()
+
+        borrowing.save(update_fields=["actual_return_date"])
+
+        borrowing.book.inventory += 1
+        borrowing.book.save(update_fields=["inventory"])
+
+        return Response(
+            BorrowingSerializer(borrowing).data,
+            status=status.HTTP_200_OK,
+        )
