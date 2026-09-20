1adef86ff7206825f47f5bef887efbbde116685f
Oleh M <oleh.murachov28@gmail.com>
2026-09-20 23:57:09 +0200

    feat: add borrowing filtering

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,5 +1,6 @@
 from django.db import transaction
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
 from rest_framework.permissions import IsAuthenticated
 
 from books.models import Book
@@ -11,8 +12,29 @@ from borrowings.serializers import (
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
-    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = Borrowing.objects.all()
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        else:
+            user_id = self.request.query_params.get("user_id")
+
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+
+        is_active = self.request.query_params.get("is_active")
+
+        if is_active is not None:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "create":
@@ -27,8 +49,6 @@ class BorrowingViewSet(viewsets.ModelViewSet):
         )
 
         if book.inventory == 0:
-            from rest_framework.exceptions import ValidationError
-
             raise ValidationError(
                 {"book": "This book is not available."}
             )
